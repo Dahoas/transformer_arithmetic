94f81512b2c3e1d11bd846054887912e53caa0d2
Dahoas <alexdahoas@gmail.com>
2023-04-08 02:42:11 +0000

    adding chain-of-thought
    Signed-off-by: Dahoas <alexdahoas@gmail.com>

--- data/gen_data.py
+++ data/gen_data.py
@@ -11,6 +11,41 @@ def simple_template(num1, num2, noise_level):
         summed += torch.randint(-int(summed**0.5), int(summed**0.5), (1,)).item()
     return {"prompt": f"{num1}+{num2}=", "response": str(summed)}
 
+def chain_of_thought_template(num1, num2, noise_level):
+    # TODO add noiselevel
+    str1 = str(num1)
+    str2 = str(num2)
+    
+    # pad with 0s
+    max_len = max(len(str1), len(str2))
+    iter1 = str1[::-1]
+    iter2 = str2[::-1]
+    iter1 = iter1.ljust(max_len, "0")
+    iter2 = iter2.ljust(max_len, "0")
+    
+    # iterate
+    response = ""
+    answer = ""
+    carry = 0
+    for i in range(max_len):
+        dig1 = int(iter1[i])
+        dig2 = int(iter2[i])
+        sum_i = dig1 + dig2 + carry
+        next_dig = sum_i % 10
+        carry = sum_i // 10
+        answer = str(next_dig) + answer
+        response += f"Let's add the {i} digits. "
+        response += f"{i} digit of {str1} = {dig1}. {i} digit of {str2} = {dig2}. Then, summed with carry, we have {sum_i}. "
+        response += f"{i} digit is {next_dig}. The carry is now {carry}. "
+        response += f"The result so far is {answer}. "
+    if carry > 0:
+        answer = str(carry) + answer
+        response += f"Finally, we have leftover carry of {carry}. Then, the result is {answer}. "
+
+    response += f"The final answer is {answer}."
+
+    return {"prompt": f"{num1}+{num2}=", "response": response}
+
 # Generate dataset summing up to ten digit numbers
 def gen_noisy_dataset(noise_level, file_name, prompt_template, num_samples=100000, max_num=int(1e10)):
     terms = torch.randint(0, max_num, (num_samples, 2)).tolist()
@@ -31,10 +66,10 @@ def gen_noisy_dataset(noise_level, file_name, prompt_template, num_samples=10000
 
 
 if __name__ == "__main__":
-    noise_levels = [0.0, 0.001, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.15, 0.2]
+    noise_levels = [0.0]#[0.0, 0.001, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.1, 0.15, 0.2]
     dataset_dir = "datasets/"
 
-    prompt_template = simple_template
+    prompt_template = chain_of_thought_template
     for noise_level in noise_levels:
         file_name = dataset_dir + "additions_{}_{}".format(noise_level, prompt_template.__name__)
         gen_noisy_dataset(noise_level=noise_level, file_name=file_name, prompt_template=prompt_template)
