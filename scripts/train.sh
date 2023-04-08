15f691e491b88e4445404a3f24e5b7d92d3f8c56
maia-iyer <maia.raj.iyer@gmail.com>
2023-04-08 04:18:40 +0000

    chain of thought script
    Signed-off-by: maia-iyer <maia.raj.iyer@gmail.com>

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-deepspeed --num_gpus=8 train.py --data_path data/datasets/additions_0.005_simple_template \
+deepspeed --num_gpus=8 train.py --data_path data/datasets/additions_0.0_chain_of_thought_template \
 --deepspeed configs/ds_config.json
